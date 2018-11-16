a98c41fd82fca9bbdeff7a39e05f9c288824ca1f
Steven Capuzzi <stevenczz@gmail.com>
2018-11-16 18:04:34 -0500

    changes made to index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
-const express = require ('express');
-const axios = require ('axios');
+const express = require('express');
+const axios = require('axios');
 const app = express();
+
+let PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log("Listening on port " + PORT));
