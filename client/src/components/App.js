80075c4e366ebbe7f463073fc17c6eb680e4bf9f
Steven Capuzzi <stevenczz@gmail.com>
2018-11-17 15:38:25 -0500

    just pulling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ import MealList from './MealList';
 import TotalCalories from './TotalCalories';
 import MoreInfo from './MoreInfo';
 import axios from 'axios';
-import { key, appID } from '../config';
+import { key, appID } from '../../../config';
 
 class App extends Component {
 
@@ -17,7 +17,7 @@ class App extends Component {
 }
 
   async componentDidMount(){
-      let {data} = await axios('https://api.nutritionix.com/v1_1/search/taco?results=0%3A1&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&');
+      let {data} = await axios('https://api.nutritionix.com/v1_1/search/taco?results=0%3A1&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=');
     
       this.setState({
           nutri: data
