22ef2125780ae8ea7b1aec15d2a6ee8956067f73
Maya Kano <mayakanoweb@gmail.com>
2018-11-17 18:28:23 -0800

    changes

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -4,6 +4,13 @@ class Input extends Component {
     render() {
         return (
             <div>THIS IS THE INPUT COMPONENT</div>
+            <form>
+            <label>
+              Pick Your Item:
+              <input type="text" name="name" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         )
     }
 }
