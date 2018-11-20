4c7733e425fe46c1a08bb8897168c0f3ce28905c
Steven Capuzzi <stevenczz@gmail.com>
2018-11-20 16:48:01 -0500

    cursor over + button

--- client/src/components/MealList.js
+++ client/src/components/MealList.js
@@ -41,7 +41,7 @@ class MealList extends Component {
 
             <ul id = "meal-list">
                 <li id="mealitem">  {this.state.name.charAt(0).toUpperCase() + this.state.name.slice(1)}: {this.state.totalCurrentCalories} Calories. ({this.state.totalCurrentServing} {this.state.unit}) 
-                <a onClick={() => {this.props.editCalories(this.state.calories); this.addServing()}}> <img class="plus-sign" src="../../plus-sign.png"/></a></li>
+                <a onClick={() => {this.props.editCalories(this.state.calories); this.addServing()}}> <img class="plus-sign" id="plus" src="../../plus-sign.png"/></a></li>
 
             </ul>
 
